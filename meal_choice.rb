a944ed919d301c14a74817f6fe68f11b243bda85
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-15 14:46:13 +0000

    Automatically backed up by Learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,9 +1,6 @@
 # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
 
-require "spec_helper"
-
-def meal_choice (chosen = "meat")
+def meal_choice (choice = "meat")
+    puts choice
 end
-
-  puts chosen
