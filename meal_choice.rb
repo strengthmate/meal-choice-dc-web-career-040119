89adf87ec9963881d27843a12ee052fdfbe615a5
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-15 14:33:38 +0000

    Automatically backed up by Learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,2 +1,10 @@
 # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
+
+def meal_choice(chosen = "meat")
+  puts chosen
+end
+
+meal_choice
+
+end
